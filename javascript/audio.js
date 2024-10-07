a5fa87f2bfd9dd700fce9f3704c4b7c16232889f
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-10-07 14:53:40 +0530

    integrate web audio api context nodes part 2

--- javascript/audio.js
+++ javascript/audio.js
@@ -24,3 +24,30 @@ export const AudioEngine = {
   synthStartTime: 0,
   synthVirtualTime: 0,
   synthTempo: 120,
+  synthStep: 0,
+  nextNoteTime: 0,
+  lookahead: 25.0, // ms
+  scheduleAheadTime: 0.1, // sec
+
+  // Default Equalizer Frequencies
+  eqFrequencies: [60, 230, 910, 4000, 14000],
+
+  /**
+   * Initializes the Audio Engine. MUST be triggered by a user action.
+   */
+  init() {
+    if (this.ctx) return;
+
+    // Create AudioContext
+    const AudioContextClass = window.AudioContext || window.webkitAudioContext;
+    this.ctx = new AudioContextClass();
+
+    // Create Audio Element for streaming local files
+    this.audioEl = new Audio();
+    this.audioEl.crossOrigin = "anonymous";
+    this.sourceNode = this.ctx.createMediaElementSource(this.audioEl);
+
+    // Create Equalizer Filters (5-band peaking)
+    this.eqFilters = this.eqFrequencies.map((freq) => {
+      const filter = this.ctx.createBiquadFilter();
+      filter.type = 'peaking';
