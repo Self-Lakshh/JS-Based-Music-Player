53da84223c6eda7d3044671bf54347468a00b011
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-12-12 19:51:07 +0530

    map keyboard shortcuts accessibility keys part 1

---
+++ javascript/shortcuts.js
@@ -0,0 +1,6 @@
+/**
+ * BeatStream Keyboard Shortcuts Manager
+ * Exposes accessible hotkeys for hands-free player control.
+ * Binds keys to actions while ignoring inputs when typing in forms.
+ */
+
